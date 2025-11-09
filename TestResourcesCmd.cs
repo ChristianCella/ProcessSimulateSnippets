d87a794f7d884940337f7fa7475777c819fd03f6
ChristianCella <christian.cella@polimi.it>
2025-11-09 17:54:25 +0100

    Added some new methods

--- TestResourcesCmd.cs
+++ TestResourcesCmd.cs
@@ -34,19 +34,27 @@ namespace ProcessSimulateSnippets
         public override void Execute(object cmdParams)
         {
 
-            double[] translation = new double[] { 0.0, 0.0, 0.0 };
+            double[] translation = new double[] { 200.0, 0.0, 0.0 };
             double[] rotation = new double[] { 0.0, 0.0, 0.0 };
             double[] joint_values = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
             bool invisible = false;
+            double device_pos = 1400; 
 
             // This class is defined in TxResources.cs
             TxResources item_resource = new TxResources();
             TxResources robot_resource = new TxResources();
+            TxResources line_resource = new TxResources();
             //robot_resource.PlaceResource("GoFa12", translation, rotation);
-            robot_resource.SetTCP("GoFa12", "TOOLFRAME");
-            //ur5_resource.SetJointValue("UR5", joint_values);
-
+            //robot_resource.SetTCP("GoFa12", "TCPF_crate"); //TCPF_crate, TOOLFRAME
+            //robot_resource.UnMountToolGripper("GoFa12", "Collaborative_gripper", "collaborative_tool_station");
+            //robot_resource.MountToolGripper("GoFa12", "Collaborative_gripper", "tool_holder_offset", "BASEFRAME_collaborative");
+            //robot_resource.DisplayMountedTools("GoFa12");
+            //robot_resource.ImposeRobotConfig("GoFa12", "fr6");
+            //robot_resource.SetJointValue("GoFa12", joint_values);
             //item_resource.ChangeVisibility("YAOSC_cube", invisible);
+            // line_resource.CreateDevicePose("Line", device_pos, "TestPose");
+            //robot_resource.ComputeJacobian();
+            robot_resource.PlaceResourceAccordingToFrame("Crate_3", "crate_low_on_slider_station");
 
         }
     }
--- TxResources.cs
+++ TxResources.cs
@@ -20,10 +20,81 @@ namespace ProcessSimulateSnippets
             TxTransformation.TxRotationType.RPY_XYZ);
             resource.AbsoluteLocation = Rotation;
 
-            // Move the base of a certain quantity		
+            // Linear translations	
             var position = new TxTransformation(resource.LocationRelativeToWorkingFrame);
             position.Translation = new TxVector(translation[0], translation[1], translation[2]);
             resource.LocationRelativeToWorkingFrame = position;
+
+            TxApplication.RefreshDisplay();
+        }
+
+        public void PlaceResourceAccordingToFrame(string resource_name, string frame_name)
+        {
+            // Move the tool to the station 
+            var frame_var = GetLocatableResource(frame_name);
+            TxTransformation frame_pose = frame_var.AbsoluteLocation;
+            double[] translation = new double[] { frame_pose.Translation.X, frame_pose.Translation.Y, frame_pose.Translation.Z };
+            double[] rotation = new double[] { frame_pose.RotationRPY_XYZ.X, frame_pose.RotationRPY_XYZ.Y, frame_pose.RotationRPY_XYZ.Z };
+
+            PlaceResource(resource_name, translation, rotation);
+        }
+
+        public void MountToolGripper(string robot_name, string tool_name, string flange_frame, string tool_frame)
+        {
+            // Get the robot
+            var robot = GetLocatableResource(robot_name);
+            var rob = robot as TxRobot; // Needed to access the method 'TCPF'
+
+            // Get the tool
+            var tool = GetLocatableResource(tool_name);
+
+            // Get the frame on the robot flange
+            var flange = GetLocatableResource(flange_frame);
+            TxTransformation flange_pose = flange.AbsoluteLocation;
+
+            // Get the frame on the tool
+            var tool_target = GetLocatableResource(tool_frame);
+            TxTransformation tool_pose = tool_target.AbsoluteLocation;
+
+            // Mount the tool
+            rob.MountTool(tool, flange_pose, tool_pose);
+            TxApplication.RefreshDisplay();
+
+        }
+
+        public void UnMountToolGripper(string robot_name, string tool_name, string station_name)
+        {
+            // Get the robot
+            var robot = GetLocatableResource(robot_name);
+            var rob = robot as TxRobot; // Needed to access the method 'TCPF'
+
+            // Get the tool
+            var tool = GetLocatableResource(tool_name);
+
+            rob.UnmountTool(tool);
+            TxApplication.RefreshDisplay();
+
+            // Move the tool to the station 
+            PlaceResourceAccordingToFrame(tool_name, station_name);
+        }
+
+        public void DisplayMountedTools(string robot_name)
+        {
+            // Get the robot
+            var robot = GetLocatableResource(robot_name);
+            var rob = robot as TxRobot; // Needed to access the method 'TCPF'
+
+            TxObjectList mounted_tools = rob.MountedTools;
+
+            // Build message with all mounted tool names
+            string message = "Mounted Tools:\n";
+            for (int i = 0; i < mounted_tools.Count; i++)
+            {
+                var tool = mounted_tools[i];
+                message += string.Format("{0}. {1}\n", i + 1, tool.Name);
+            }
+
+            TxMessageBox.Show(message, "Mounted Tools", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public void SetJointValue(string resource_name, double[] joint_values)
@@ -67,6 +138,41 @@ namespace ProcessSimulateSnippets
             TxApplication.RefreshDisplay();
         }
 
+        // Make TCPF coincident with a specific frame (and move the robot accordingly)
+        public void ImposeRobotConfig(string robot_name, string frame_name)
+        {
+            // Get the target frame (i.e. where the screw is)
+            TxFrame target_frame = TxApplication.ActiveDocument.GetObjectsByName(frame_name)[0] as TxFrame;
+
+            // Define a variable for the inverse kinematics
+            TxRobotInverseData inv = new TxRobotInverseData(target_frame.AbsoluteLocation);
+
+            // Get the robot
+            var robot = GetLocatableResource(robot_name);
+            var rob = robot as TxRobot; // Needed to access the method 'TCPF'
+
+            // Compute the inverse kinematics
+            bool proceed = rob.DoesInverseExist(inv);
+            if (proceed == true)
+            {
+                var poses = rob.CalcInverseSolutions(inv);
+
+                // Set a certain pose (i.e. the first one)
+                var poseData = poses[0] as TxPoseData;
+
+                // Impose a specific configuration to the robot		
+                rob.CurrentPose = poseData;
+
+                // Refresh the display		
+                TxApplication.RefreshDisplay();
+            }
+            else
+            {
+                TxMessageBox.Show("Impossible to compute IK", "Watch out!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
         // Make an object visible or invisible
         public void ChangeVisibility(string resource_name, bool invisible)
         {
@@ -87,6 +193,101 @@ namespace ProcessSimulateSnippets
             TxApplication.RefreshDisplay();
         }
 
+        // Create a pose for a device (i.e. a line, NOT A ROBOT)
+        public void CreateDevicePose(string device_name, double device_pos, string pose_name)
+        {
+            var dev = GetLocatableResource(device_name);
+            TxDevice device = dev as TxDevice;
+
+            // Create a new pose
+            TxPoseData openposeData = new TxPoseData();
+            ArrayList openarraylist = new ArrayList();
+            openarraylist.Add(device_pos);
+            openposeData.JointValues = openarraylist;
+            TxPoseCreationData NewPose = new TxPoseCreationData(pose_name, openposeData);
+            TxPose new_base_pose = device.CreatePose(NewPose);
+        }
+
+        // Compute the Jacobian of the robot
+        public void ComputeJacobian()
+        {
+            // Get transformation frames
+            TxFrame DH0 = TxApplication.ActiveDocument.GetObjectsByName("BASEFRAME")[0] as TxFrame;
+            var Frame0 = new TxTransformation(DH0.LocationRelativeToWorkingFrame);
+
+            TxFrame DH1 = TxApplication.ActiveDocument.GetObjectsByName("fr1")[0] as TxFrame;
+            var Frame1 = new TxTransformation(DH1.LocationRelativeToWorkingFrame);
+
+            TxFrame DH2 = TxApplication.ActiveDocument.GetObjectsByName("fr2")[0] as TxFrame;
+            var Frame2 = new TxTransformation(DH2.LocationRelativeToWorkingFrame);
+
+            TxFrame DH3 = TxApplication.ActiveDocument.GetObjectsByName("fr3")[0] as TxFrame;
+            var Frame3 = new TxTransformation(DH3.LocationRelativeToWorkingFrame);
+
+            TxFrame DH4 = TxApplication.ActiveDocument.GetObjectsByName("fr4")[0] as TxFrame;
+            var Frame4 = new TxTransformation(DH4.LocationRelativeToWorkingFrame);
+
+            TxFrame DH5 = TxApplication.ActiveDocument.GetObjectsByName("fr5")[0] as TxFrame;
+            var Frame5 = new TxTransformation(DH5.LocationRelativeToWorkingFrame);
+
+            TxFrame DH6 = TxApplication.ActiveDocument.GetObjectsByName("TOOLFRAME")[0] as TxFrame;
+            var Frame6 = new TxTransformation(DH6.LocationRelativeToWorkingFrame);
+
+            // Get joint positions in meters
+            var x1 = Frame1[0, 3] / 1000; var y1 = Frame1[1, 3] / 1000; var z1 = Frame1[2, 3] / 1000;
+            var x2 = Frame2[0, 3] / 1000; var y2 = Frame2[1, 3] / 1000; var z2 = Frame2[2, 3] / 1000;
+            var x3 = Frame3[0, 3] / 1000; var y3 = Frame3[1, 3] / 1000; var z3 = Frame3[2, 3] / 1000;
+            var x4 = Frame4[0, 3] / 1000; var y4 = Frame4[1, 3] / 1000; var z4 = Frame4[2, 3] / 1000;
+            var x5 = Frame5[0, 3] / 1000; var y5 = Frame5[1, 3] / 1000; var z5 = Frame5[2, 3] / 1000;
+            var x6 = Frame6[0, 3] / 1000; var y6 = Frame6[1, 3] / 1000; var z6 = Frame6[2, 3] / 1000;
+
+            // Define Z axes (rotation vectors)
+            double[] Z0 = { Frame0[0, 2], Frame0[1, 2], Frame0[2, 2] };
+            double[] Z1 = { Frame1[0, 2], Frame1[1, 2], Frame1[2, 2] };
+            double[] Z2 = { Frame2[0, 2], Frame2[1, 2], Frame2[2, 2] };
+            double[] Z3 = { Frame3[0, 2], Frame3[1, 2], Frame3[2, 2] };
+            double[] Z4 = { Frame4[0, 2], Frame4[1, 2], Frame4[2, 2] };
+            double[] Z5 = { Frame5[0, 2], Frame5[1, 2], Frame5[2, 2] };
+
+            // Position vectors
+            double[] p0 = { 0.0, 0.0, 0.0 };
+            double[] p1 = { x1, y1, z1 };
+            double[] p2 = { x2, y2, z2 };
+            double[] p3 = { x3, y3, z3 };
+            double[] p4 = { x4, y4, z4 };
+            double[] p5 = { x5, y5, z5 };
+            double[] p6 = { x6, y6, z6 }; // End-effector position
+            double[] p = p6;
+
+            // Compute linear part of Jacobian
+            double[] result0 = CrossProduct(Z0, Subtract(p, p0));
+            double[] result1 = CrossProduct(Z1, Subtract(p, p1));
+            double[] result2 = CrossProduct(Z2, Subtract(p, p2));
+            double[] result3 = CrossProduct(Z3, Subtract(p, p3));
+            double[] result4 = CrossProduct(Z4, Subtract(p, p4));
+            double[] result5 = CrossProduct(Z5, Subtract(p, p5));
+
+            // Build Jacobian matrix J (6x6)
+            double[,] J = {
+            { result0[0], result1[0], result2[0], result3[0], result4[0], result5[0] },
+            { result0[1], result1[1], result2[1], result3[1], result4[1], result5[1] },
+            { result0[2], result1[2], result2[2], result3[2], result4[2], result5[2] },
+            { Z0[0], Z1[0], Z2[0], Z3[0], Z4[0], Z5[0] },
+            { Z0[1], Z1[1], Z2[1], Z3[1], Z4[1], Z5[1] },
+            { Z0[2], Z1[2], Z2[2], Z3[2], Z4[2], Z5[2] }
+        };
+
+            // Compute standard determinant
+            double detJ = CalculateDeterminant(J);
+            TxMessageBox.Show("Determinant of Jacobian: det(J) = " + detJ.ToString("G6"), "Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Compute Yoshikawa manipulability index: w = sqrt(det(J*Jᵗ))
+            double[,] JJt = MultiplyMatrixByTranspose(J);
+            double detJJt = CalculateDeterminant(JJt);
+            double w = Math.Sqrt(Math.Abs(detJJt));
+            TxMessageBox.Show("Yoshikawa Manipulability Index: w = sqrt(det(J*Jt) = " + w.ToString("G6"), "Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
         // ------------------------------- Auxiliary methods
 
         public ITxLocatableObject GetLocatableResource(string resource_name)
@@ -102,6 +303,76 @@ namespace ProcessSimulateSnippets
             TxObjectList selectedObjects = TxApplication.ActiveSelection.GetItems();
             selectedObjects = TxApplication.ActiveDocument.GetObjectsByName(resource_name);
             return selectedObjects[0] as ITxPlcLogicResource;
+        }
+
+        // Utility: Cross product of 3D vectors
+        static double[] CrossProduct(double[] a, double[] b)
+        {
+            return new double[] {
+            a[1] * b[2] - a[2] * b[1],
+            a[2] * b[0] - a[0] * b[2],
+            a[0] * b[1] - a[1] * b[0]
+        };
+        }
+
+        // Utility: Vector subtraction
+        static double[] Subtract(double[] a, double[] b)
+        {
+            return new double[] {
+            a[0] - b[0],
+            a[1] - b[1],
+            a[2] - b[2]
+        };
+        }
+
+        // Utility: Multiply matrix by its transpose: JJᵗ = J * Jᵗ
+        static double[,] MultiplyMatrixByTranspose(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            double[,] result = new double[rows, rows];
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < rows; j++)
+                {
+                    double sum = 0.0;
+                    for (int k = 0; k < cols; k++)
+                        sum += matrix[i, k] * matrix[j, k];
+                    result[i, j] = sum;
+                }
+
+            return result;
+        }
+
+        // Recursive determinant calculator (Laplace expansion)
+        static double CalculateDeterminant(double[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+            if (size != matrix.GetLength(1))
+                throw new ArgumentException("Matrix must be square.");
+
+            if (size == 1)
+                return matrix[0, 0];
+
+            double result = 0.0;
+
+            for (int j = 0; j < size; j++)
+            {
+                double[,] minor = new double[size - 1, size - 1];
+
+                for (int k = 1; k < size; k++)
+                {
+                    for (int l = 0, m = 0; l < size; l++)
+                    {
+                        if (l != j)
+                            minor[k - 1, m++] = matrix[k, l];
+                    }
+                }
+
+                result += (j % 2 == 0 ? 1 : -1) * matrix[0, j] * CalculateDeterminant(minor);
+            }
+
+            return result;
         }
 
     }
